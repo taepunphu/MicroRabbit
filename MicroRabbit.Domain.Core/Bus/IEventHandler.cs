7574c8bdd80d6f2b7cecf8ef34327645e3e44a2f
taepunphu <kiattisak.pa@outlook.com>
2021-06-19 16:40:29 +0700

    implement interface Domain.Core

---
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler where TEvent : Event
+    {
+        Task Handler(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+    }
+}
